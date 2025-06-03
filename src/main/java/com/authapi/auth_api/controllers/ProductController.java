82f01981c0381b5d996c7dba685ac765efb3e895
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-06-03 15:04:33 -0300

    Testada primeiro get de produtos - automatico

--- src/main/java/com/authapi/auth_api/controllers/ProductController.java
+++ src/main/java/com/authapi/auth_api/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.authapi.auth_api.controllers;
 
 import com.authapi.auth_api.domain.product.Product;
 import com.authapi.auth_api.domain.product.ProductRequestDTO;
+import com.authapi.auth_api.domain.product.ProductResponseDTO;
 import com.authapi.auth_api.repositories.ProductRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -27,6 +29,11 @@ public class ProductController {
         Product newProduct = new Product(body);
         this.repository.save(newProduct);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping 
+    public ResponseEntity<?> getAllProducts() {
+        return ResponseEntity.ok(this.repository.findAll().stream().map(ProductResponseDTO::new).toList());
     }
     
 
