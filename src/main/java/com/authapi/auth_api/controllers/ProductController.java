52266d7fcea3ba9e3cadfb7a88b9c98bfe8fddb1
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-06-03 14:08:09 -0300

    criado o post de procuct

--- src/main/java/com/authapi/auth_api/controllers/ProductController.java
+++ src/main/java/com/authapi/auth_api/controllers/ProductController.java
@@ -1,14 +1,14 @@
 package com.authapi.auth_api.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.authapi.auth_api.domain.product.Product;
 import com.authapi.auth_api.domain.product.ProductRequestDTO;
 import com.authapi.auth_api.repositories.ProductRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
 
 import jakarta.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
