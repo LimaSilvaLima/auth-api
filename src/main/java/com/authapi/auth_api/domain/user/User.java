642ceabab19a2ee3e2f4a81b475f86623f36ac4b
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-06-04 08:42:27 -0300

    definidos port - ocultar pilha de erros - fixados user e password

--- src/main/java/com/authapi/auth_api/domain/user/User.java
+++ src/main/java/com/authapi/auth_api/domain/user/User.java
@@ -1,5 +1,10 @@
 package com.authapi.auth_api.domain.user;
 
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,13 +21,22 @@ import lombok.NoArgsConstructor;;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String login;
     private String password;
-    private String role;
+    private UserRole role;
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+    @Override
+    public String getUsername() {
+       return login;
+    }
 
 }
